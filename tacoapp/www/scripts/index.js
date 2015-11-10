48661520e355a8e314a79109f17c5f7c91b86389
Newton Jain <newton.jain@gmail.com>
2015-11-09 16:33:45 -0800

    Made more changes

--- tacoapp/www/scripts/index.js
+++ tacoapp/www/scripts/index.js
@@ -1,21 +1,73 @@
-(function () {
-    "use strict";
+// (function () {
+//     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+//     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
+//     function onDeviceReady() {
+//         // Handle the Cordova pause and resume events
+//         document.addEventListener( 'pause', onPause.bind( this ), false );
+//         document.addEventListener( 'resume', onResume.bind( this ), false );
         
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-    };
+//         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+//     };
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+//     function onPause() {
+//         // TODO: This application has been suspended. Save application state here.
+//     };
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+//     function onResume() {
+//         // TODO: This application has been reactivated. Restore application state here.
+//     };
+// } )();
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
